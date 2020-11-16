3857b5ffd1633fa749388ad416eaa98ddb192f57
nherreradev <38512342+nherreradev@users.noreply.github.com>
2020-11-16 20:41:15 -0300

    se soluciona el ingreso del date en la BD

--- model/RegistroModel.php
+++ model/RegistroModel.php
@@ -14,7 +14,7 @@ class RegistroModel
 //        $date = date_create($f_nac);
 //        $f_nac = date_format($date,"Y/m/d");
         $result = $this->database->ejecutarQuery("insert into empleado(usuario,password,dni,f_nac)
-			        VALUES('$usuario', '$password', $dni, STR_TO_DATE('$f_nac', '%d-%m-%Y'))");
+			        VALUES('$usuario', '$password', $dni, STR_TO_DATE('$f_nac', '%Y-%m-%d'))");
          return $result;
            }
 
