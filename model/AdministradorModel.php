b352e6c66d538811e0a44265f8007a38a8aa8f0c
nherreradev <nherrera3276@gmail.com>
2020-11-18 16:46:23 -0300

    se corrige error que se genero al modificar la pk de id_usuario a usuario en la tabla empleado. No se estaba asignando el rol correctamente desde la vista del administrador

--- model/AdministradorModel.php
+++ model/AdministradorModel.php
@@ -10,16 +10,16 @@ class AdministradorModel
         $this->db = $db;
     }
 
-    public function buscarUsuarioPorId($idUsuario){
-        return $this->db->query("SELECT * FROM empleado emp left join rol rol ON rol.id_rol =  emp.id_rol where id_usuario = '$idUsuario'");
+    public function buscarUsuarioPorId($usuario){
+        return $this->db->query("SELECT * FROM empleado emp left join rol rol ON rol.id_rol =  emp.id_rol where usuario = '$usuario'");
     }
 
     public function asignarRol($id_rol, $usuario){
         return $this->db->ejecutarQuery("UPDATE empleado SET id_rol = '$id_rol' WHERE usuario = '$usuario'");
     }
 
-    public function deleteUsuario($idUsuario){
-        return $this->db->ejecutarQuery("delete from empleado where id_usuario = '$idUsuario'");
+    public function deleteUsuario($usuario){
+        return $this->db->ejecutarQuery("delete from empleado where usuario = '$usuario'");
     }
 
     public function traerTodosLosUsuariosSinRol(){
@@ -36,7 +36,7 @@ class AdministradorModel
 
     public function modificarUnUsuario($idUsuario,$usuario,$dni,$f_nac,$id_rol){
         return $this->db->ejecutarQuery("UPDATE empleado SET usuario = '$usuario' , dni = $dni, f_nac = STR_TO_DATE('$f_nac', '%Y-%m-%d'), id_rol = '$id_rol' 
-                                        WHERE id_usuario = '$idUsuario'");
+                                        WHERE usuario = '$idUsuario'");
     }
 
     public function traerTodosLosRoles(){
