0d3db6f21c87f4d52eaad7de995c7c0bf7887613
nherreradev <nherrera3276@gmail.com>
2020-11-20 11:47:27 -0300

    se soluciona modificar usuario

--- model/LoginModel.php
+++ model/LoginModel.php
@@ -14,8 +14,8 @@ class LoginModel
         return $this->database->query("SELECT * FROM empleado WHERE usuario = '$nombre' and password = '$password'");
     }
 
-    public function buscarEmpleado($usuario){
-        return $this->database->query("SELECT * FROM empleado LEFT JOIN rol ON empleado.id_rol = rol.id_rol WHERE usuario = '$usuario'");
+    public function buscarEmpleado($usuario,$password){
+        return $this->database->query("SELECT * FROM empleado LEFT JOIN rol ON empleado.id_rol = rol.id_rol WHERE usuario = '$usuario' and password = '$password'");
     }
 
     public function traerTodosLosRoles(){
